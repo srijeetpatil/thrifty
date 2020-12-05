a186e536cf7cb1c9542bcd2e31bb84d5c23f1021
srijeex <srijeetpatil7@gmail.com>
2020-12-05 13:38:16 +0530

    new added

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import '../styles/globals.css'
+import {Provider} from 'react-redux';
+import {ConfigureStore} from '../redux/configureStore';
+
+const store = ConfigureStore();
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  )  
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,9 @@
 import Question from './question';
-import {Provider} from 'react-redux';
-import {ConfigureStore} from '../redux/configureStore';
-
-const store = ConfigureStore();
 
 export default function Home() {  
   return (
-      <div>
-        <Provider store={store}>
-          <Question/>
-        </Provider>          
+      <div>        
+          <Question/>                 
       </div>    
   )
 }
